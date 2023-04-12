dad18724a4f8285cf780aeb83adaf64f5c536804
wattup4rs <gideonwatt@gmail.com>
2023-04-12 00:41:00 -0700

    man

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,6 @@
  * create_file - creates file
  * @filename: filename.
  * @text_content: content writen in the file.
- *
  * Return: 1 if it success else -1 .
  */
 int create_file(const char *filename, char *text_content)
@@ -16,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (file == -1)
 		return (-1);
